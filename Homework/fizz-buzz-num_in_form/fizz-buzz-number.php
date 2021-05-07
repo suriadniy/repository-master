35ae601bd6d7991816953d4b783a2d34b8db99eb
suriadniy <vsuriadniy@gmail.com>
2021-05-07 23:59:26 +0300

    fizz-buzz in form

---
+++ Homework/fizz-buzz-num_in_form/fizz-buzz-number.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+$fizz = intval($_POST['fizz']);
+$buzz = intval($_POST['buzz']);
+$num = intval($_POST['number']);
+
+$i = 1;
+
+while ($i <= $num) {
+	if ($i % $fizz == 0 && $i % $buzz == 0) {
+			echo "FB\n";
+		} elseif ($i % $buzz == 0) { 
+	          echo "B\n";
+		  } elseif ($i % $fizz == 0) {
+			    echo "F\n";	
+			} else {
+			     echo "$i\n";
+			  }
+			    $i++;
+}	
+
+exit();
