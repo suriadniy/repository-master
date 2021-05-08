992ef588399c93de9f192829e06405c6eaff3187
suriadniy <vsuriadniy@gmail.com>
2021-05-08 11:42:43 +0300

    merging files

--- Homework/hw9/homework9-read_write.php
+++ Homework/hw9/homework9-read_write.php
@@ -1,5 +1,24 @@
 <?php
 
+function read(&$a) {
+
+$a = fopen('file.txt', 'r+');
+
+	while (! feof($a)) {
+		$s = fgets($a);
+		echo $s;
+	}
+
+fclose($a);
+
+}
+
+$file = fopen('file.txt', 'r+');
+
+read($file);
+
+
+
 function write(&$w) {
 	$w = fopen('file.txt', 'r+');
 	$text = "Еще 1 строка !\n";
