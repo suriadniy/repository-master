81bd33d9179ce9ef596b637e248ef38c000ba102
suriadniy <vsuriadniy@gmail.com>
2021-05-12 11:24:25 +0300

    final change

--- Homework/hw9/first_level.php
+++ Homework/hw9/first_level.php
@@ -1,17 +1,56 @@
 <?php
 
+// функция для чтения
+
 $way = '/home/vladislav/alevel_git/file.txt';
 
     function read_file($way) {
-        
+
         $open_file = fopen($way, 'r+');
 
             while (!feof($open_file)) {
             
                 $text = fgets($open_file);
-                echo $text.'<br>';                             
+                echo $text;                             
             }
         fclose($open_file);
             
     }
-        read_file($way);
+       read_file($way);
+
+
+
+
+
+
+// функция для записи и создания нового файла через строку из верхней функции
+
+$way = '/home/vladislav/alevel_git/file.txt';
+
+$path_to_writing = '/home/vladislav/';
+
+    function write_file($way,$path_to_writing) {
+
+    	$count = 1;
+
+        $open_file = fopen($way, 'r+');
+
+            while (!feof($open_file)) {
+            
+                $text = fgets($open_file);
+
+                $write_file = fopen("$path_to_writing/half",'a+');
+
+                if ($count % 2 !==0) {
+
+                	fwrite($write_file, $text);
+                }
+
+                $count++;                            
+            }
+
+        fclose($open_file);
+        fclose($write_file);
+            
+    }
+       write_file($way,$path_to_writing);
